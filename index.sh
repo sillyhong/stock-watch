31ad99c20c7a1464a6387d2908f6ab25c38a67a1
liangweihong1 <liangweihong@npt.sg>
2025-02-04 13:09:21 +0800

    feat: cron update

---
+++ index.sh
@@ -0,0 +1,9 @@
+ curl http://localhost:3008/api/15-rsi-watch/hk
+ curl http://localhost:3008/api/5-rsi-watch/hk
+ curl http://localhost:3008/api/day-rsi-watch/hk
+ curl http://localhost:3008/api/15-rsi-watch/a
+ curl http://localhost:3008/api/5-rsi-watch/a
+ curl http://localhost:3008/api/day-rsi-watch/a
+ curl http://localhost:3008/api/15-rsi-watch/us
+ curl http://localhost:3008/api/5-rsi-watch/us
+ curl http://localhost:3008/api/day-rsi-watch/us
