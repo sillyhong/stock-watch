16b50a66f469594b6f8ca88b6a77f104f9557c09
liangweihong1 <liangweihong@npt.sg>
2025-02-18 19:47:55 +0800

    feat: update

--- index.sh
+++ index.sh
@@ -1,9 +1,13 @@
  curl http://localhost:3008/api/15-rsi-watch/hk
  curl http://localhost:3008/api/5-rsi-watch/hk
  curl http://localhost:3008/api/day-rsi-watch/hk
+ curl http://localhost:3008/api/backtrend/15-rsi/hk
  curl http://localhost:3008/api/15-rsi-watch/a
  curl http://localhost:3008/api/5-rsi-watch/a
  curl http://localhost:3008/api/day-rsi-watch/a
+ curl http://localhost:3008/api/backtrend/15-rsi/a
  curl http://localhost:3008/api/15-rsi-watch/us
  curl http://localhost:3008/api/5-rsi-watch/us
  curl http://localhost:3008/api/day-rsi-watch/us
+ curl http://localhost:3008/api/day-rsi-watch/us
+ curl http://localhost:3008/api/backtrend/15-rsi/us
