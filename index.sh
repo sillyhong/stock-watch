ff7635d721985fc71ecb3535cfa03093a2564827
liangweihong1 <liangweihong@npt.sg>
2025-03-16 19:43:30 +0800

    feat: update

--- index.sh
+++ index.sh
@@ -1,3 +1,5 @@
+git pull
+pm2 restart stock-watch-dev 
  curl http://localhost:3008/api/15-rsi-watch/hk
  curl http://localhost:3008/api/5-rsi-watch/hk
  curl http://localhost:3008/api/day-rsi-watch/hk
@@ -7,7 +9,7 @@
  curl http://localhost:3008/api/day-rsi-watch/a
  curl http://localhost:3008/api/backtrend/15-rsi/a
  curl http://localhost:3008/api/15-rsi-watch/us
- curl http://localhost:3008/api/5-rsi-watch/us
+#  curl http://localhost:3008/api/5-rsi-watch/us
  curl http://localhost:3008/api/day-rsi-watch/us
  curl http://localhost:3008/api/day-rsi-watch/us
  curl http://localhost:3008/api/backtrend/15-rsi/us
